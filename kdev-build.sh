768df3c72f84af82f84bf54ce115c405e0b0c7f0
yifengyou <842056007@qq.com>
2026-09-21 23:22:25 +0800

    update workflow
    Signed-off-by: yifengyou <842056007@qq.com>

--- kdev-build.sh
+++ kdev-build.sh
@@ -2,9 +2,38 @@
 
 set -xe
 
+mkdir keys
+# We don't really need a usable PK, so just generate a public key for it and discard the private key
+openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Rockchip Platform Key/" -keyout /dev/null -outform DER -out keys/pk.cer -days 7300 -nodes -sha256
+curl -L https://go.microsoft.com/fwlink/?LinkId=321185 -o keys/ms_kek.cer
+curl -L https://go.microsoft.com/fwlink/?linkid=321192 -o keys/ms_db1.cer
+curl -L https://go.microsoft.com/fwlink/?linkid=321194 -o keys/ms_db2.cer
+curl -L https://uefi.org/sites/default/files/resources/dbxupdate_arm64.bin -o keys/arm64_dbx.bin
+
+export EDK2_SECUREBOOT_FLAGS=" \
+  -D DEFAULT_KEYS=TRUE \
+  -D PK_DEFAULT_FILE=keys/pk.cer \
+  -D KEK_DEFAULT_FILE1=keys/ms_kek.cer \
+  -D DB_DEFAULT_FILE1=keys/ms_db1.cer \
+  -D DB_DEFAULT_FILE2=keys/ms_db2.cer \
+  -D DBX_DEFAULT_FILE1=keys/arm64_dbx.bin \
+  -D SECURE_BOOT_ENABLE=TRUE"
+
+export EDK2_BUILD_FLAGS=" \
+  ${EDK2_SECUREBOOT_FLAGS}"
+
 cp -a bdy-g98-dts/rk3588-bdy-g98.dtb devicetree/vendor/
 
 ./build.sh --clean
-./build.sh --device bdy-g98 --release Release
+./build.sh --device bdy-g98 --release Release --edk2-flags "${EDK2_BUILD_FLAGS}"
 
+TIMESTAMP=$(date +%Y%m%d)
+mkdir -p release
+sudo cp -a RK3588_NOR_FLASH.img release/RK3588_NOR_FLASH.img
+sudo cp -a RK3588_NOR_FLASH.img release/RK3588_NOR_FLASH-${TIMESTAMP}.img
+sudo cp -a RK3588_NOR_FLASH.img RKDevTool_Release_v3.37/
+sudo rar a release/RK3588_NOR_FLASH_With_RKDevTool.rar RKDevTool_Release_v3.37
+sudo rar a release/RK3588_NOR_FLASH_With_RKDevTool-${TIMESTAMP}.rar RKDevTool_Release_v3.37
+
+ls -alh release
 
